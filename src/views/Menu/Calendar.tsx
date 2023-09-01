8c8ba8a36e06f2bd623b2fd5b72726a9ff0fe93f
Hanseul-C <86642982+Hanseul-C@users.noreply.github.com>
2023-09-01 17:06:29 +0900

    [NW-57] 일정 등록 진행중

--- src/views/Menu/Calendar.tsx
+++ src/views/Menu/Calendar.tsx
@@ -1,16 +1,14 @@
-import { useEffect, useState } from 'react'
-import Modal from 'react-modal';
+import { useEffect } from 'react'
 import CalendarView from '@/components/shared/CalendarView'
 import Container from '@/components/shared/Container'
-// import EventDialog, { EventParam } from './components/EventDialog'
-// import reducer, {
-//     getEvents,
-//     updateEvent,
-//     setSelected,
-//     openDialog,
-//     useAppDispatch,
-//     useAppSelector,
-// } from './store'
+import reducer, {
+    getEvents,
+    updateEvent,
+    setSelected,
+    openDialog,
+    useAppDispatch,
+    useAppSelector,
+} from './store'
 import { injectReducer } from '@/store'
 import cloneDeep from 'lodash/cloneDeep'
 import dayjs from 'dayjs'
@@ -19,105 +17,105 @@ import type {
     EventClickArg,
     DateSelectArg,
 } from '@fullcalendar/core'
-import CalendarEdit from './CalendarEdit';
-import { Dialog } from '@/components/ui';
+import CalendarEdit, { EventParam } from './CalendarEdit'
 
-// injectReducer('crmCalendar', reducer)
+injectReducer('crmCalendar', reducer)
 
 const Calendar = () => {
-    const [editOpen, setEditOpen] = useState(false)
-
-    const openEdit = () => {
-        console.log("열기")
-        setEditOpen(!editOpen)
-    }
-    // const dispatch = useAppDispatch()
-    // const events = useAppSelector((state) => state.crmCalendar.data.eventList)
+    const dispatch = useAppDispatch()
+    const events = useAppSelector((state) => state.crmCalendar.data.eventList)
 
     useEffect(() => {
-        // dispatch(getEvents())
+        dispatch(getEvents())
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     const onCellSelect = (event: DateSelectArg) => {
-        // const { start, end } = event
-        // dispatch(
-        //     setSelected({
-        //         type: 'NEW',
-        //         start: dayjs(start).format(),
-        //         end: dayjs(end).format(),
-        //     })
-        // )
-        // dispatch(openDialog())
-        console.log("선택")
-        setEditOpen(!editOpen)
+        const { start, end } = event
+        dispatch(
+            setSelected({
+                type: 'NEW',
+                start: dayjs(start).format(),
+                end: dayjs(end).format(),
+            })
+        )
+        dispatch(openDialog())
     }
 
     const onEventClick = (arg: EventClickArg) => {
-        // const { start, end, id, title, extendedProps } = arg.event
-        // dispatch(
-        //     setSelected({
-        //         type: 'EDIT',
-        //         eventColor: extendedProps.eventColor,
-        //         title,
-        //         start,
-        //         end,
-        //         id,
-        //     })
-        // )
-        // dispatch(openDialog())
-        openEdit();
-        return (
-            <Dialog isOpen={editOpen}><CalendarEdit /></Dialog>
+        const { start, end, id, title, extendedProps } = arg.event
+        dispatch(
+            setSelected({
+                type: 'EDIT',
+                eventColor: extendedProps.eventColor,
+                title,
+                start,
+                end,
+                id,
+            })
         )
+        dispatch(openDialog())
     }
 
-    // const onSubmit = (data: EventParam, type: string) => {
-    //     let newEvents = cloneDeep(events)
-    //     if (type === 'NEW') {
-    //         newEvents.push(data)
-    //     }
+    const onSubmit = (data: EventParam, type: string) => {
+        const newEvents = cloneDeep(events)
+        if (type === 'NEW') {
+            newEvents.push(data)
+        }
 
-    //     if (type === 'EDIT') {
-    //         newEvents = newEvents.map((event) => {
-    //             if (data.id === event.id) {
-    //                 event = data
-    //             }
-    //             return event
-    //         })
-    //     }
-    //     // dispatch(updateEvent(newEvents))
-    // }
+        // if (type === 'EDIT') {
+        //     newEvents = newEvents.map((event) => {
+        //         if (data.id === event.id) {
+        //             event = data
+        //         }
+        //         return event
+        //     })
+        // }
+        dispatch(updateEvent(newEvents))
+    }
 
-    // const onEventChange = (arg: EventDropArg) => {
-    //     const newEvents = cloneDeep(events).map((event) => {
-    //         if (arg.event.id === event.id) {
-    //             const { id, extendedProps, start, end, title } = arg.event
-    //             event = {
-    //                 id,
-    //                 start: dayjs(start).format(),
-    //                 end: dayjs(end).format(),
-    //                 title,
-    //                 eventColor: extendedProps.eventColor,
-    //             }
-    //         }
-    //         return event
-    //     })
-    //     dispatch(updateEvent(newEvents))
-    // }
+    const onEventChange = (arg: EventDropArg) => {
+        const newEvents = cloneDeep(events).map((event) => {
+            if (arg.event.id === event.id) {
+                const {
+                    id,
+                    groom,
+                    groomContact,
+                    groomEmail,
+                    bride,
+                    brideContact,
+                    brideEmail,
+                } = arg.event
+                event = {
+                    id,
+                    groom,
+                    groomContact,
+                    groomEmail,
+                    bride,
+                    brideContact,
+                    brideEmail,
+                    // start: dayjs(start).format(),
+                    // end: dayjs(end).format(),
+                    // title,
+                    // eventColor: extendedProps.eventColor,
+                }
+            }
+            return event
+        })
+        dispatch(updateEvent(newEvents))
+    }
 
     return (
-        <Container className="h-full relative ">
-           {editOpen && <CalendarEdit onOpenAlert={openEdit} />}
+        <Container className="h-full">
             <CalendarView
                 editable
                 selectable
-                // events={events}
-                eventClick={openEdit}
+                events={events}
+                eventClick={onEventClick}
                 select={onCellSelect}
-                // eventDrop={onEventChange}
+                eventDrop={onEventChange}
             />
-            {/* <EventDialog /> */}
+            <CalendarEdit submit={onSubmit} />
         </Container>
     )
 }
---
+++ src/views/Menu/store/calendarSlice.ts
@@ -0,0 +1,98 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import ApiService from '@/services/ApiService'
+// import { apiGetCrmCalendar } from '@/services/CrmService'
+
+export async function apiGetCrmCalendar<T>() {
+    return ApiService.fetchData<T>({
+        url: '/crm/calendar',
+        method: 'get',
+    })
+}
+
+type Event = {
+    id: string
+    groom: string
+    groomContact: string
+    groomEmail: string
+    bride: string
+    brideContact: string
+    brideEmail: string
+    weddingDate: Date
+    // eventColor: string
+    // groupId?: undefined
+}
+
+type Events = Event[]
+
+type GetCrmCalendarResponse = Events
+
+export type CalendarState = {
+    loading: boolean
+    eventList: Events
+    dialogOpen: boolean
+    selected: {
+        type: string
+        groom: string
+        groomContact: string
+        groomEmail: string
+        bride: string
+        brideContact: string
+        brideEmail: string
+        weddingDate: Date
+    } & Partial<Event>
+}
+
+export const SLICE_NAME = 'crmCalendar'
+
+export const getEvents = createAsyncThunk(
+    SLICE_NAME + '/getEvents',
+    async () => {
+        const response = await apiGetCrmCalendar<GetCrmCalendarResponse>()
+        return response.data
+    }
+)
+
+const initialState: CalendarState = {
+    loading: false,
+    eventList: [],
+    dialogOpen: false,
+    selected: {
+        type: '',
+        groom: '',
+        groomContact: '',
+        groomEmail: '',
+        bride: '',
+        brideContact: '',
+        brideEmail: '',
+        weddingDate: new Date(),
+    },
+}
+
+const calendarSlice = createSlice({
+    name: `${SLICE_NAME}/state`,
+    initialState,
+    reducers: {
+        updateEvent: (state, action) => {
+            state.eventList = action.payload
+        },
+        openDialog: (state) => {
+            state.dialogOpen = true
+        },
+        closeDialog: (state) => {
+            state.dialogOpen = false
+        },
+        setSelected: (state, action) => {
+            state.selected = action.payload
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(getEvents.fulfilled, (state, action) => {
+            state.eventList = action.payload
+        })
+    },
+})
+
+export const { updateEvent, openDialog, closeDialog, setSelected } =
+    calendarSlice.actions
+
+export default calendarSlice.reducer
